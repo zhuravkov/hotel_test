33f31c2e59e6afb34fd8154551f929dabea2890c
sergey <zhuravkov.ss@yandex.ru>
2022-05-29 22:31:00 +0500

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
-import './App.css';
+import style from './App.module.css';
+import bg_img from './assets/images/Rectangle_32.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className={style.App}>
+        <div className={style.header__container}>
+          <div className={style.header__top}>
+            Лого и ссылки
+          </div>
+          <div className={style.header__center}>
+            CENTER
+          </div>
+          <div className={style.header__bottom}>
+            bottom
+          </div>
+        </div>
+
+
+        <div className={style.find__block}>
+          <div className={style.find__block__left}>
+            Получите подборку лучших номеров по выгодной цене
+          </div>
+          <div className={style.find__block__right}>
+            search components 
+          </div>
+        </div>
     </div>
   );
 }
