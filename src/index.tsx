c835a943c9067f24369687195dace7c7ca51b9a9
sergey zhuravkov <zhuravkov.ss@yandex.ru>
2022-06-26 10:41:23 +0500

    modify for working with django integrated in project

--- src/index.tsx
+++ src/index.tsx
@@ -15,11 +15,11 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
